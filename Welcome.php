3e8f71bc7a3de5f45b04bb31c7d2967272125536
Pratham4417 <khairnarprathamesh00@gmail.com>
2026-05-11 23:36:24 +0530

    Add files via upload

---
+++ Welcome.php
@@ -0,0 +1,8 @@
+<?php
+// PHP script to display Welcome message and current date & time
+
+echo "<h2>Welcome</h2>";
+
+// Display current date and time
+echo "Current Date and Time: " . date("d-m-Y h:i:s A");
+?>
